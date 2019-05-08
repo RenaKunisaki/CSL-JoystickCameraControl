4babf4626c653b79514c860c809f16ffd4f924a8
RenaKunisaki <hyperhacker@gmail.com>
2019-05-08 13:25:41 -0400

    bump version

--- JoystickCamera/JoystickCamera.cs
+++ JoystickCamera/JoystickCamera.cs
@@ -10,9 +10,9 @@ using System.Linq;
 
 namespace JoystickCamera {
 	public class JoystickCamera: ThreadingExtensionBase, IUserMod {
-		public string Name => "Joystick Camera Control (v2.0)";
+		public string Name => "Joystick Camera Control (v2.00.01)";
 		public string Description => "Use a joystick, extra mice, etc to control the camera.";
-		public readonly int Version = 20000; //Mod version (2.00.00)
+		public readonly int Version = 20001; //Mod version (2.00.01)
 		public readonly int ConfigVersion = 20000; //config file format version
 		public readonly float PI_OVER_180 = Mathf.PI / 180f;
 		protected List<JoystickInputDef> inputs;
